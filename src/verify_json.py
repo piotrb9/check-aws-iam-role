8725c9932315a855f92db2dab265669d98a9ea39
piotrb <piotrbanko@me.com>
2024-03-30 00:17:07 +0100

    Add JSONDecodeError and FileNotFoundError error handling

--- src/verify_json.py
+++ src/verify_json.py
@@ -2,13 +2,21 @@ import json
 
 
 def verify_iam_role_policy(file_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
 
-    # Iterate over the policies in the IAM Role
-    for statement in data.get("Statement", []):
-        # Check if 'Resource' is a string and equals "*"
-        if isinstance(statement.get("Resource"), str) and statement.get("Resource") == "*":
-            return False
+        # Iterate over the policies in the IAM Role
+        for statement in data.get("Statement", []):
+            # Check if 'Resource' is a string and equals "*"
+            if isinstance(statement.get("Resource"), str) and statement.get("Resource") == "*":
+                return False
 
-    return True
+        return True
+
+    except json.JSONDecodeError:
+        print("Error decoding JSON.")
+        return False
+    except FileNotFoundError:
+        print("File not found.")
+        return False
