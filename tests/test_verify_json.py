09fff586a5943c50ba3bae6b219e1a57d46a3e6a
piotrb <piotrbanko@me.com>
2024-03-30 23:49:39 +0100

    Fix "File not found" error

--- tests/test_verify_json.py
+++ tests/test_verify_json.py
@@ -1,50 +1,66 @@
 import unittest
 from src.verify_json import verify_iam_role_policy
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestVerifyIAMRolePolicy(unittest.TestCase):
-
     def test_valid_policy(self):
-        self.assertFalse(verify_iam_role_policy("test_data/valid_role_policy.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/valid_role_policy.json")
+        self.assertFalse(verify_iam_role_policy(json_path))
 
     def test_empty_file(self):
-        self.assertTrue(verify_iam_role_policy("test_data/empty_file.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/empty_file.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_no_asterisk(self):
-        self.assertTrue(verify_iam_role_policy("test_data/no_asterisk.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/no_asterisk.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_no_resource_field(self):
-        self.assertTrue(verify_iam_role_policy("test_data/no_resource_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/no_resource_field.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_other_text(self):
-        self.assertTrue(verify_iam_role_policy("test_data/other_text.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/other_text.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_no_statement_field(self):
-        self.assertTrue(verify_iam_role_policy("test_data/no_statement_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/no_statement_field.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_valid_policy_extra_fields(self):
-        self.assertFalse(verify_iam_role_policy("test_data/valid_role_policy_extra_fields.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/valid_role_policy_extra_fields.json")
+        self.assertFalse(verify_iam_role_policy(json_path))
 
     def test_no_file(self):
-        self.assertTrue(verify_iam_role_policy("test_data/no_file.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/no_file.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_no_policy_document_field(self):
-        self.assertTrue(verify_iam_role_policy("test_data/no_policy_document_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/no_policy_document_field.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_number_resource_field(self):
-        self.assertTrue(verify_iam_role_policy("test_data/number_resource_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/number_resource_field.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_valid_list_resource_field(self):
-        self.assertFalse(verify_iam_role_policy("test_data/valid_list_resource_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/valid_list_resource_field.json")
+        self.assertFalse(verify_iam_role_policy(json_path))
 
     def test_empty_list_resource_field(self):
-        self.assertTrue(verify_iam_role_policy("test_data/empty_list_resource_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/empty_list_resource_field.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_invalid_json_structure(self):
-        self.assertTrue(verify_iam_role_policy("test_data/invalid_json_structure.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/invalid_json_structure.json")
+        self.assertTrue(verify_iam_role_policy(json_path))
 
     def test_valid_single_list_resource_field(self):
-        self.assertFalse(verify_iam_role_policy("test_data/valid_single_list_resource_field.json"))
+        json_path = os.path.join(BASE_DIR, "test_data/valid_single_list_resource_field.json")
+        self.assertFalse(verify_iam_role_policy(json_path))
 
 
 if __name__ == '__main__':
