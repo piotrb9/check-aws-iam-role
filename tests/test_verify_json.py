643b8b80586eee1b62c53592951e6d6aded3ed4d
piotrb <piotrbanko@me.com>
2024-03-30 21:06:45 +0100

    Add test_empty_file

--- tests/test_verify_json.py
+++ tests/test_verify_json.py
@@ -4,5 +4,8 @@ from src.verify_json import verify_iam_role_policy
 
 class TestVerifyIAMRolePolicy(unittest.TestCase):
 
-    def test_empty_policy(self):
-        self.assertTrue(verify_iam_role_policy("test_data/valid_role_policy.json"))
+    def test_valid_policy(self):
+        self.assertFalse(verify_iam_role_policy("test_data/valid_role_policy.json"))
+
+    def test_empty_file(self):
+        self.assertTrue(verify_iam_role_policy("test_data/empty_file.json"))
